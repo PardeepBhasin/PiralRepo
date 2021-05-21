79546150d02d5ea0876f82678779488eb82a0499
Pardeep Bhasin <pardeep.bhasin@nagarro.com>
2021-05-21 12:48:11 +0530

    Added nuget exe

--- sample-pilet-service/teamcity/gitversion.ps1
+++ sample-pilet-service/teamcity/gitversion.ps1
@@ -7,3 +7,9 @@ Write-Host $commandline
 
 $json = packages\gitversion.commandline.3.6.5\tools\GitVersion.exe
 write-host $json
+
+#The Out-String cmdlet converts the objects that PowerShell manages into an array of strings
+#The ConvertFrom-Json cmdlet converts a JavaScript Object Notation (JSON) formatted string to a custom PSCustomObject object that has a property for each field in the JSON string.
+$version = $json | out-string | ConvertFrom-Json
+Write-Host $version
+Write-Host "##teamcity[buildNumber '$($version.FullSemVer)']"
